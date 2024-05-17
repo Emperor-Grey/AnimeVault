76fce2a0a5bcc342f8f65a301bc9d76851ad7e27
King Grey <kinggrey9959@gmail.com>
2024-05-17 11:52:19 +0530

    Loading and Server Components Learnt

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import LoadMore from "../components/LoadMore";
 import {fetchAnime} from "@/app/action";
 
 async function Home() {
-
-    const data = await fetchAnime(2);
+    const data = await fetchAnime(1);
 
     return (<main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
         <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,19 +1,41 @@
+"use client"
 import Image from "next/image";
+import {useInView} from "react-intersection-observer";
+import {useEffect, useState} from "react";
+import {fetchAnime} from "@/app/action";
+import AnimeCard, {AnimeProp} from "@/components/AnimeCard";
+
+let page: number = 2;
 
 function LoadMore() {
+    const {ref, inView} = useInView();
+    const [data, setData] = useState<AnimeProp[]>([]);
+
+    useEffect(() => {
+        if (inView) {
+            fetchAnime(page).then((res) => {
+                setData([...data, ...res]);
+                page++;
+            })
+        }
+    }, [inView, data]);
+
     return (<>
-            <section className="flex justify-center items-center w-full">
-                <div>
-                    <Image
-                        src="./spinner.svg"
-                        alt="spinner"
-                        width={56}
-                        height={56}
-                        className="object-contain"
-                    />
-                </div>
-            </section>
-        </>);
+        <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+            {data.map((item: AnimeProp, index: number) => (<AnimeCard key={item.id} anime={item} index={index}/>))}
+        </section>
+        <section className="flex justify-center items-center w-full">
+            <div ref={ref}>
+                <Image
+                    src="./spinner.svg"
+                    alt="spinner"
+                    width={56}
+                    height={56}
+                    className="object-contain"
+                />
+            </div>
+        </section>
+    </>);
 }
 
 export default LoadMore;
